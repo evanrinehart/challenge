bf45cfd4060d691e625abc613e20b10c5ae98dd0
EvanR <evanrinehart@gmail.com>
2017-10-05 18:19:41 -0500

    more tests

--- Tests.hs
+++ Tests.hs
@@ -4,6 +4,7 @@ module Tests where
 import Test.QuickCheck
 import qualified Data.Text as T
 import Data.Monoid ((<>))
+import Data.Either
 
 import Types
 import Encoder
@@ -80,3 +81,64 @@ prop_printPrints = edit (Print 4) ("example", []) == (Just 'm', ("example", []))
 prop_undoUndoes = edit Undo ("exam", ["example"]) == (Nothing, ("example", []))
 
 
+prop_goodFileDoesParse = parseInput goodFile == Right goodData where
+  goodFile = 
+    T.unlines [
+      "4",
+      "1 example",
+      "2 5",
+      "3 1",
+      "4"
+    ]
+  goodData = [Append "example", Delete 5, Print 1, Undo]
+
+prop_badFileNoParse1 = isLeft (parseInput badFile1) where
+  badFile1 = T.unlines [
+      "1",
+      "2 1000001" -- delete too many
+    ]
+
+
+prop_badFileNoParse2 = isLeft (parseInput badFile2) where
+  badFile2 = T.unlines [
+      "2", -- wrong number of lines
+      "2 1000000" 
+    ]
+
+prop_badFileNoParse3 = isLeft (parseInput badFile3) where
+  badFile3 = T.unlines [
+      "2", -- wrong number of lines
+      "1 foo",
+      "2 2",
+      "1 ruit"
+    ]
+
+prop_badFileNoParse4 = isLeft (parseInput badFile4) where
+  badFile4 = T.unlines [
+      "1",
+      "0 wrong" -- op 0 is invalid
+    ]
+
+prop_badFileNoParse5 = isLeft (parseInput badFile5) where
+  badFile5 = T.unlines [
+      "2",
+      "1 fruit",
+      "2 0" -- no delete 0
+    ]
+
+prop_badFileNoParse6 = isLeft (parseInput badFile6) where
+  badFile6 = T.unlines [
+      "2",
+      "1 fruit",
+      "3 0" -- no print 0
+    ]
+
+prop_goodFileEdits = runEditor ops ("",[]) == ("e", ("example",[""])) where
+  Right ops = parseInput goodFile
+  goodFile = T.unlines [
+      "4",
+      "1 example",
+      "2 5",
+      "3 1",
+      "4"
+    ]
